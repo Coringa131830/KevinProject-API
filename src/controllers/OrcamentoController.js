33ca0ef482b3c1facb5f46fab6baba8b29f60cad
Antoniel Magalhães <antoniel2210@gmail.com>
2020-08-17 15:10:59 -0300

    Change orçamento model

--- src/controllers/OrcamentoController.js
+++ src/controllers/OrcamentoController.js
@@ -1,74 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Cliente = mongoose.model('Cliente');
-const Vendedor = mongoose.model('Vendedor');
-const Orcamento = mongoose.model('Orcamento');
+const Cliente = mongoose.model("Cliente");
+const Vendedor = mongoose.model("Vendedor");
+const Orcamento = mongoose.model("Orcamento");
 
 async function create(req, res) {
+	const { userId, body } = req;
+	const { itens, cliente, valor } = body;
 
-    const { userId, body } = req;
-    const { itens, cliente } = body;
+	const _cliente = await Cliente.findOne({ cpf: cliente });
+	if (!_cliente.active) return res.json({ err: "This customer isn't active" });
 
-    const _cliente = await Cliente.findOne({ cpf: cliente });
-    if (_cliente.ative) return res.json({ err: "This customer isn't active" });
+	const itensListParsed = itens.split(";");
 
-    const itensListParsed = itens.split(";");
+	const orcamento = await Orcamento.create({ cliente: _cliente, vendedor: userId, itens: itensListParsed, valor });
 
-    const orcamento = await Orcamento.create({ cliente: _cliente, vendedor: userId, itens: itensListParsed });
-
-    return res.json({ orcamento });
-    
+	return res.json({ orcamento });
 }
 
+async function showPending(req, res) {
+	const orcamentos = await Orcamento.find({ approved: false });
 
-async function showPending(req,res) {
-
-    const orcamentos = await Orcamento.find({ approved: false });
-
-    return res.json({ orcamentos });
-
+	return res.json({ orcamentos });
 }
 
-async function showApproved(req,res) {
+async function showApproved(req, res) {
+	const orcamentos = await Orcamento.find({ approved: true });
 
-    const orcamentos = await Orcamento.find({ approved: true });
-
-    return res.json({ orcamentos });
-
+	return res.json({ orcamentos });
 }
 
 async function approve(req, res) {
+	const { _id } = req.params;
 
-    const { _id } = req.params;
+	const _orcamento = await Orcamento.findById({ _id });
 
-    const _orcamento = await Orcamento.findById({ _id });
+	if (!_orcamento) return res.json({ err: "Orcamento not found!" });
 
-    if(!_orcamento) return res.json({ err: "Orcamento not found!" });
+	const nOrcamento = await Orcamento.findOneAndUpdate({ _id }, { approved: true });
 
-    const nOrcamento = await Orcamento.findOneAndUpdate({ _id }, {approved: true});
-
-    return res.json(nOrcamento);
-
+	return res.json(nOrcamento);
 }
 
-
 async function orcamentoDetails(req, res) {
-    
-    const { _id } = req.params;
+	const { _id } = req.params;
 
-    const _orcamento = await Orcamento.findById({ _id });
+	const _orcamento = await Orcamento.findById({ _id });
 
-    if(!_orcamento) return res.json({ err: "Orcamento not found!" });
+	if (!_orcamento) return res.json({ err: "Orcamento not found!" });
 
-    const { name, email, phone, cpf, address, active } = await Cliente.findOne({ _id: _orcamento.cliente });
-    const vendedor = await Vendedor.findOne({ _id: _orcamento.vendedor });
+	const { name, email, phone, cpf, address, active } = await Cliente.findOne({ _id: _orcamento.cliente });
+	const vendedor = await Vendedor.findOne({ _id: _orcamento.vendedor });
 
-    return res.json({  _orcamento, 
-        cliente: { name, email, phone, cpf, address }, 
-        vendedor 
-    });
-
-}   
-
+	return res.json({ _orcamento, cliente: { name, email, phone, cpf, address }, vendedor });
+}
 
 module.exports = { create, showPending, approve, showApproved, orcamentoDetails };
--- src/models/orcamento.js
+++ src/models/orcamento.js
@@ -1,27 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const OrcamentoSchema = new mongoose.Schema({
-    
-    cliente: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-    },
-    vendedor: {
-        type: mongoose.Types.ObjectId,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    itens: {
-        type: [String],
-    },
-    approved: {
-        type: Boolean,
-        default: false,
-    },
-
+	cliente: {
+		type: mongoose.Types.ObjectId,
+		required: true,
+	},
+	vendedor: {
+		type: mongoose.Types.ObjectId,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+	itens: {
+		type: [String],
+	},
+	valor: {
+		type: Number,
+	},
+	approved: {
+		type: Boolean,
+		default: false,
+	},
 });
 
-mongoose.model('Orcamento', OrcamentoSchema);
+mongoose.model("Orcamento", OrcamentoSchema);
