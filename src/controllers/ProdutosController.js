7aee620c122ac2bbc3222f8b6c38fa9fd336f8fc
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2020-09-14 12:42:06 -0300

    add route to search for products

--- src/controllers/ProdutosController.js
+++ src/controllers/ProdutosController.js
@@ -14,4 +14,21 @@ async function listAll( req, res ){
 
 }
 
-module.exports = { listAll }
+async function search( req, res ){ 
+
+    const { product } = req.query;
+
+    const headers = {
+            'Authorization-Token': "8abb3a4021525ec6642f8ad1256cc27892038ff1172b024003fa4a96b1332604e4be5d43c0533a582ac02ba255153ef049ae8433e93036d842244f836b19339afcef5bbbc8ab936b2d36bd22541fdb87c99bfb8b9f82b9e34506455b0b8a72cd8d3bef38d574b1525ceaeb57dc5f1e39e3a31a3ac501b2b80b2a5d6d0778d6e1",
+            User: "financeiro@mkdistribuidora.com",
+            App: "Pedidos",
+    }
+    const response = await axios.get(`http://api.sigecloud.com.br/request/Produtos/Pesquisar?nome=${product}`, {
+        headers: headers,
+    });
+
+    return res.json({produtos: response.data})
+
+}
+
+module.exports = { listAll, search }
--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const ProdutosController = require('../controllers/ProdutosController');
 
 router.get('/all', ProdutosController.listAll);
+router.get('/search', ProdutosController.search);
 
 module.exports = router;
