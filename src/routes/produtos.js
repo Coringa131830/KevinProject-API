740aac2b1bb20c370c0e73354a2bb9300711ebe6
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2020-09-14 17:20:48 -0300

    change searches route from get to post

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const ProdutosController = require('../controllers/ProdutosController');
 
 router.get('/all', ProdutosController.listAll);
-router.get('/search', ProdutosController.search);
+router.post('/search', ProdutosController.search);
 
 module.exports = router;
