a801c69146f2772d543a0c4724a52681e31cbdff
Antoniel Magalhães <antoniel2210@gmail.com>
2020-10-08 12:14:06 -0300

    add: send price on search a product

--- src/controllers/ProdutosController.js
+++ src/controllers/ProdutosController.js
@@ -17,7 +17,6 @@ async function listAll( req, res ){
 async function search( req, res ){ 
 
     const { product } = req.body;
-
     const headers = {
             'Authorization-Token': "8abb3a4021525ec6642f8ad1256cc27892038ff1172b024003fa4a96b1332604e4be5d43c0533a582ac02ba255153ef049ae8433e93036d842244f836b19339afcef5bbbc8ab936b2d36bd22541fdb87c99bfb8b9f82b9e34506455b0b8a72cd8d3bef38d574b1525ceaeb57dc5f1e39e3a31a3ac501b2b80b2a5d6d0778d6e1",
             User: "financeiro@mkdistribuidora.com",
@@ -30,7 +29,7 @@ async function search( req, res ){
     const produtos = response.data;
 
     produtos.forEach((val, idx, arr) => {                                               
-        arr[idx] = { value: val.Nome, label: val.Nome };                      
+        arr[idx] = { value: val.Nome, label: val.Nome, price: val.PrecoVenda };                      
     }, produtos);
 
     return res.json({produtos})
